c1a5122e3c0c1c6faf94266696dd9ede2fd33894
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-05-14 20:09:44 +0430

    javafx login layout added!

---
+++ src/sample/Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML TextField usernameField;
+    @FXML PasswordField passwordField;
+
+    public void handleRegisterButtonAction(ActionEvent actionEvent) {
+
+    }
+
+    public void handleLoginButtonAction(ActionEvent actionEvent) {
+
+    }
+
+    public void handleScoreboardButtonAction(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Login");
+        Scene scene = new Scene(root, 480, 360);
+        scene.getStylesheets().add("stylesheet/style.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
