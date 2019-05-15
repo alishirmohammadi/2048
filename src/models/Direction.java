bea67b3cbd155e524ead949de769056202a8eca5
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-05-15 12:33:33 +0430

    Game models added

---
+++ src/models/Direction.java
@@ -0,0 +1,19 @@
+public enum Direction {
+    UP, RIGHT, LEFT, DOWN;
+
+    public static int getDeltaY(Direction direction) {
+        if(direction == LEFT)
+            return -1;
+        if(direction == RIGHT)
+            return 1;
+        return 0;
+    }
+
+    public static int getDeltaX(Direction direction) {
+        if(direction == UP)
+            return -1;
+        if(direction == DOWN)
+            return 1;
+        return 0;
+    }
+}
---
+++ src/models/Game.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+
+public class Game {
+    private int[][] map;
+    private int n;
+    private static Random random = new Random();
+
+    public Game(int n) {
+        map = new int[n][n];
+        this.n = n;
+    }
+
+    public void insert() {
+        int x, y;
+        do {
+            x = random.nextInt(n);
+            y = random.nextInt(n);
+        } while(map[x][y] != 0);
+        map[x][y] = random.nextInt(2) * 2 + 2;
+    }
+
+    private void applyGravity(Direction direction) {
+        int deltaX = Direction.getDeltaX(direction);
+        int deltaY = Direction.getDeltaY(direction);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                try {
+                    if(map[i + deltaX][j + deltaY] == 0) {
+                        map[i + deltaX][j + deltaY] = map[i][j];
+                        map[i][j] = 0;
+                    }
+                } catch (ArrayIndexOutOfBoundsException ignored) {}
+
+            }
+        }
+    }
+
+    public void move(Direction direction) {
+        for(int i = 0; i < n; i++)
+            applyGravity(direction);
+        int deltaX = Direction.getDeltaX(direction);
+        int deltaY = Direction.getDeltaY(direction);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                try {
+                    if(map[i][j] == map[i + deltaX][j + deltaY]) {
+                        map[i][j] = map[i][j] * 2;
+                        map[i + deltaX][j + deltaY] = 0;
+                    }
+                } catch (ArrayIndexOutOfBoundsException ignored) {}
+            }
+        }
+        for(int i = 0; i < n; i++)
+            applyGravity(direction);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                stringBuilder.append(String.format("%d\t", map[i][j]));
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
